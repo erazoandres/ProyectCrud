2e5ee4c069f5a1690d25970e87d1aa131a13538c
Andres Erazo <erazoandres14@gmail.com>
2023-02-09 03:55:09 -0500

    enlazado php con ver los posts

--- php/viewPost.php
+++ php/viewPost.php
@@ -1,6 +1,16 @@
 <?php
     include "../php/header.php";
     include "../php/footer.php";
+    include "../php/conection.php";
+
+
+    
+    $sentencia = $conn->prepare("SELECT * FROM post");
+    $sentencia->execute();
+
+    $resultado = $sentencia->fetchAll();
+    
+
     ?>
 
 
@@ -39,7 +49,6 @@
     
         </div>
     </div>
-
 
     <div class="container">
         <div class="">
@@ -84,6 +93,28 @@
                     </a>
                 </div> -->
             </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row mb-2">
+            <?php foreach($resultado as $fila){ ?>
+            <div class="col-md-6">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div class="col p-4 d-flex flex-column position-static">
+                    <strong class="d-inline-block mb-2 text-primary">World</strong>
+                    <h3 class="mb-0"><?php echo $fila["title"] ?></h3>
+                    <div class="mb-1 text-muted"><?php echo $fila["date"] ?></div>
+                    <p class="card-text mb-auto" style="line-clamp:2 ; -webkit-line-clamp:2 ; overflow:hidden ; display:-webkit-box;-webkit-box-orient:vertical" ><?php echo $fila["content"] ?></p>
+                    <a href="#" class="stretched-link">Continue reading</a>
+                    </div>
+                    <div class="col-auto d-none d-lg-block">
+                    <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" style="--darkreader-inline-fill: #404447;" data-darkreader-inline-fill=""></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em" style="--darkreader-inline-fill: #d2cfcb;" data-darkreader-inline-fill="">Thumbnail</text></svg>
+
+                    </div>
+                </div>
+                </div>
+            <?php } ?>
         </div>
     </div>
 </main>
