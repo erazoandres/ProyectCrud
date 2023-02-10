452b5a551d5ed54fb47f33a9a5c81090123f21ef
Andres Erazo <erazoandres14@gmail.com>
2023-02-09 20:24:28 -0500

    Card mejoradas

--- php/index.php
+++ php/index.php
@@ -55,23 +55,22 @@
     <div class="container">
         <div class="">
             <div class="d-flex flex-wrap my-4">
-
-                <div class="col bg-secondary px-5 py-2 text-center mx-2">
+                <div class="col bg-secondary p-5 text-center">
                     
-                    <a class="d-block pt-4 pb-2" href="" style="color:gold">
+                    <a class="d-block " href="" style="color:gold">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                             <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                         </svg>
                     </a>
-                    <h2 class="text-white pt-2 pb-md-3 pb-2"> <strong> Lorem, ipsum dolor.</strong></h2>
+                    <h2 class="text-white pt-2 pb-md-3 pb-2 mx-2"> <strong> Lorem, ipsum dolor.</strong></h2>
                     <p class="blockquote-footer">Lorem, ipsum.</p>
                     <div class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, possimus. Doloremque suscipit sit a facere. Repellat tempore ut et ipsa, ducimus laudantium sapiente laboriosam cumque, perspiciatis ab illo explicabo provident?</div>
                     <a class="text-white" href="#" class="text-info">Lorem, ipsum dolor...</a>
                 </div>
 
-                <div class="col bg-secondary px-5 py-2 text-center mx-2">
+                <div class="col bg-secondary p-5 text-center mx-2">
                     
-                    <a class="d-block pt-4 pb-2" href="" style="color:gold">
+                    <a class="d-block " href="" style="color:gold">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"  fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                             <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                         </svg>
