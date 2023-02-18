3f8a76ceb022e14aac98288529a46599c68532a1
Andres Erazo <erazoandres14@gmail.com>
2023-02-18 01:46:13 -0500

    Ya se puede ver todas las imagenes

--- php/imagenes2.php
+++ php/imagenes2.php
@@ -10,4 +10,21 @@
 ?>
 
 
-<img width=300px src="data:image/png;base64,<?php echo base64_encode($res[2]["file"]) ?>" alt ="">
+<div>
+    <div>
+    <?php
+        $i = 0;
+        foreach($res as $row){
+
+            $i = $i+1;
+
+        ?>
+            <img width="300px" src="data:image/jpg;base64,<?php echo base64_encode($res[$i]["file"]) ?>" alt ="">
+        
+        <?php 
+            }
+        ?>
+    </div>
+</div>
+
+
