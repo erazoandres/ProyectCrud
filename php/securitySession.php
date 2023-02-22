8bc4fbac31f727baa4c7d32c40d7488385f6d8a0
Andres Erazo <erazoandres14@gmail.com>
2023-02-22 03:31:17 -0500

    Agregada 1era capa de seguridad que controla la duracion de la sesion x inactividad 2

--- php/securitySession.php
+++ php/securitySession.php
@@ -7,7 +7,7 @@
 //utilizamos el mismo script que antes
 if ($_SESSION["autentificado"] != "SI") {
     //si no está logueado lo envío a la página de autentificación
-    // header("Location: login.php");
+    header("Location: login.php");
 } else {
     //sino, calculamos el tiempo transcurrido
     $fechaGuardada = $_SESSION["ultimoAcceso"];
@@ -15,9 +15,10 @@ if ($_SESSION["autentificado"] != "SI") {
     $tiempo_transcurrido = (strtotime($ahora)-strtotime($fechaGuardada));
 
     //comparamos el tiempo transcurrido
-     if($tiempo_transcurrido >=300) {
+     if($tiempo_transcurrido >=5) {
      //si pasaron 10 minutos o más
       session_destroy(); // destruyo la sesión
+      session_unset(); // libero todas las variables
       header("Location: login.php"); //envío al usuario a la pag. de autenticación
       //sino, actualizo la fecha de la sesión
     }else {
